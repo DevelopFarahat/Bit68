8e24ab244603f4cff710b4646c84aab15d360379
divFarahat <mohamedfarahat366@gmail.com>
2023-03-11 14:40:19 +0200

    updating sliders

--- src/components/genaric_categories_brands/genaric_categories_brands.js
+++ src/components/genaric_categories_brands/genaric_categories_brands.js
@@ -36,9 +36,6 @@ const GenaricCategoriesBrands = ({ apiEndpointsArr,backgroundColor,imageStyles,h
   }, []);
 
   useEffect(()=>{
-    if(window.innerWidth <= 700)
-    setSlidePreoperities({slidesPerView:'auto',spaceBetween:0});
-    else setSlidePreoperities({slidesPerView:6,spaceBetween:120});
     function handleResize() {
       console.log("width: ", window.innerWidth, "px");
       if(window.innerWidth <= 700)
